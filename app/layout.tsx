2c6c07a67c1b0493f8bb72685b2e11e2df2d8549
sebastien <sebastien.savan@gmail.com>
2025-08-01 09:59:06 +0200

    change main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <head></head>
       <body className={montserrat.className + " flex flex-col min-h-screen"}>
         <Header />
-        <main className='flex justify-center items-center pt-16 flex-1 w-full'>
+        <main className='flex flex-col justify-center items-center pt-16 flex-1 w-full'>
           {" "}
           {children}
         </main>
