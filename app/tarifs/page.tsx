76364b9698c9b22fd43418cbf95ffc0422c75f14
sebastien <sebastien.savan@gmail.com>
2025-08-07 19:31:52 +0200

    add new services in constants

--- app/tarifs/page.tsx
+++ app/tarifs/page.tsx
@@ -39,76 +39,84 @@ export default function TarifsPage() {
       </h3>
       <section className='grid gap-8 md:grid-cols-2'>
         <RateCard
-          href='/'
+          href='/services/rituel-special-enfant'
           imageUrl='https://www.nefhairdydy.com/wp-content/uploads/2024/11/nefhairdydy-prestation-enfant-03.jpeg'
-          imageAlt="Arriere plan d'un salon de coiffure"
+          imageAlt='Coiffure rituel special enfant'
           title={`Rituel "Spécial Enfant" (à partir de 7 ans)`}
           description='Coaching en presentaill et en ligne'
           price='70€'
         />
         <RateCard
-          href='/'
+          href='/services/rituel-decouverte'
           imageUrl='https://www.nefhairdydy.com/wp-content/uploads/2023/10/Modele-Coupes-NefHairDydy-29-683x1024.jpg'
-          imageAlt="Arriere plan d'un salon de coiffure"
+          imageAlt='Coiffure rituel decouverte'
           title={`Rituel "Découverte" (Soin classique 1er Rdv)`}
           description='Coaching en presentaill et en ligne'
           price='110€'
         />
         <RateCard
-          href='/'
+          href='/services/rituel-retour-au-naturel-bigchop'
           imageUrl='https://www.nefhairdydy.com/wp-content/uploads/2023/10/Modele-Coupes-NefHairDydy-1.jpg'
-          imageAlt="Arriere plan d'un salon de coiffure"
+          imageAlt='Coiffure rituel retour au naturel bigchop'
           title='Rituel “Retour au Naturel” (Bigchop)'
           description='Coaching en presentaill et en ligne'
           price='110€'
         />
         <RateCard
-          href='/'
+          href='/services/rituel-soin-exfoliant-relaxant'
           imageUrl='https://www.nefhairdydy.com/wp-content/uploads/2023/10/Modele-Coupes-NefHairDydy-24-683x1024.jpg'
-          imageAlt="Arriere plan d'un salon de coiffure"
+          imageAlt='Coiffure rituel soin exfoliant relaxant'
           title='Rituel "Soin Exfoliant/Relaxant" (Pellicules et Desquamations)'
           description='Coaching en presentaill et en ligne'
           price='125€'
         />
         <RateCard
-          href='/'
+          href='/services/rituel-soin-supreme-vapeur-coupe-des-pointes'
           imageUrl='https://www.nefhairdydy.com/wp-content/uploads/2023/10/Modele-Coupes-NefHairDydy-25-1024x682.jpg'
-          imageAlt="Arriere plan d'un salon de coiffure"
+          imageAlt='Coiffure'
           title='Rituel "Soin Suprême Vapeur" - Coupe des pointes'
           description='Coaching en presentaill et en ligne'
           price='145€'
         />
         <RateCard
-          href='/'
+          href='/services/rituel-coupe-de-transformation-ou-mise-en-forme'
           imageUrl='https://www.nefhairdydy.com/wp-content/uploads/2023/10/Modele-Coupes-NefHairDydy-5-683x1024.jpg'
-          imageAlt="Arriere plan d'un salon de coiffure"
+          imageAlt='Coiffure transformation ou mise en forme'
           title={`Rituel "Coupe de Transformation"`}
           description='Coaching en presentaill et en ligne'
           price='150€'
         />
         <RateCard
-          href='/'
+          href='/services/rituel-soin-supreme-vapeur-coupe-de-transformation'
           imageUrl='https://www.nefhairdydy.com/wp-content/uploads/2023/10/Modele-Coupes-NefHairDydy-13-1024x683.jpg'
-          imageAlt="Arriere plan d'un salon de coiffure"
+          imageAlt='Coiffure rituel soin supreme vapeur coupe de transformation'
           title={`Rituel "Soin Suprême Vapeur" - Coupe de transformation ou de restructuration`}
           description='Coaching en presentaill et en ligne'
           price='180€'
         />
         <RateCard
-          href='/'
+          href='/services/rituel-coloration'
           imageUrl='https://www.nefhairdydy.com/wp-content/uploads/2023/10/Modele-Coupes-NefHairDydy-28-683x1024.jpg'
-          imageAlt="Arriere plan d'un salon de coiffure"
+          imageAlt='nefhairdydy Coiffure rituel coloration'
           title={`Rituel "Coloration"`}
           description='Coaching en presentaill et en ligne'
           price='190€'
         />
         <RateCard
-          href='/'
+          href='/services/rituel-coloration-coupe'
           imageUrl='https://www.nefhairdydy.com/wp-content/uploads/2023/10/Modele-Coupes-NefHairDydy-2-8-683x1024.jpg'
-          imageAlt="Arriere plan d'un salon de coiffure"
+          imageAlt='nefhairdydy Coiffure rituel coloration + coupe'
           title={`Rituel “Coloration + Coupe“`}
           description='Coaching en presentaill et en ligne'
           price='250€'
+        />
+        <RateCard
+          href='/services/rituel-balayage'
+          imageUrl='https://www.nefhairdydy.com/wp-content/uploads/2023/10/Modele-Coupes-NefHairDydy-2-4-683x1024.jpg'
+          imageAlt='nefhairdydy Coiffure rituel balayage'
+          title={`Rituel “Balayage“`}
+          description='Coaching en presentaill et en ligne'
+          price='290€'
         />
       </section>
     </div>
--- lib/constants.ts
+++ lib/constants.ts
@@ -43,4 +43,164 @@ Je vous propose un rendez-vous d'accompagnement personnalisé pour apprendre vo
       "Routine Capillaire",
     ],
   },
+  "rituel-coloration": {
+    name: "Rituel coloration",
+    image:
+      "https://www.nefhairdydy.com/wp-content/uploads/2023/10/Modele-Coupes-NefHairDydy-28-683x1024.jpg",
+    price: 190,
+    description: `Ce rituel, vous permet de bénéficier d'une coloration ou d'une décoloration sur vos cheveux, Dydy vous accompagne dans le choix de votre couleur et vous conseille également sur la façon de maintenir vos cheveux en pleine santé après la coloration.`,
+    content: [
+      "Entretien client",
+      "Diagnostic capillaire (Analyseur de cheveux obligatoire)",
+      "Choix de la coloration",
+      "Coloration",
+      "Shampoing",
+      "Soin",
+      "Mise en beaute",
+    ],
+  },
+  "rituel-coloration-coupe": {
+    name: `Rituel "Coloration + Coupe`,
+    image:
+      "https://www.nefhairdydy.com/wp-content/uploads/2023/10/Modele-Coupes-NefHairDydy-2-8-683x1024.jpg",
+    price: 250,
+    description: `Ce rituel, vous permet de bénéficier d'une coloration ou d'une décoloration sur vos cheveux. Dydy vous accompagne dans le choix de votre couleur, votre coupe et vous conseille également sur la façon de maintenir vos cheveux en pleine santé après la coloration.`,
+    content: [
+      "Entretien client",
+      "Diagnostic capillaire (Analyseur de cheveux obligatoire)",
+      "Coupe",
+      "Choix de la coloration",
+      "Shampoing",
+      "Soin post coloration",
+      "Mise en beauté",
+    ],
+  },
+  "rituel-balayage": {
+    name: `Rituel "Balayage"`,
+    image:
+      "https://www.nefhairdydy.com/wp-content/uploads/2023/10/Modele-Coupes-NefHairDydy-2-4-683x1024.jpg",
+    price: 290,
+    description: `Dydy vous propose une technique de balayage pour apporter de la lumière à vos cheveux, le balayage est une technique de coloration qui vous permet d’éclaicir vos longueurs en apportant une nuance à partir de votre couleur naturelle. Dydy vous accompagne dans le choix de votre coloration et vous conseille également sur la façon d’entretenir vos cheveux en pleine santé suite à votre balayage.`,
+    content: [
+      "Entretien client",
+      "Diagnostic capillaire (Analyseur de cheveux obligatoire)",
+      "Choix du type de la coloration",
+      "Shampoing",
+      "Soin post coloration",
+      "Mise en beauté",
+    ],
+  },
+  "rituel-soin-supreme-vapeur-coupe-de-transformation": {
+    name: `Rituel "Soin Suprême Vapeur" - Coupe de transformation ou de restructuration`,
+    image:
+      "https://www.nefhairdydy.com/wp-content/uploads/2023/10/Modele-Coupes-NefHairDydy-13-1024x683.jpg",
+    price: 180,
+    description: `Rituel Suprême Vapeur
+
+Ce rituel vous permet de bénéficier, d'un soin complet et profond, pour vos cheveux, du soin, à la coupe des pointes jusqu'a la coiffure, vous profiterez d'un vrai moment dé détente, avec l'expérience sous casque à vapeur pour un soin plus en profondeur.`,
+    content: [
+      "Entretien client",
+      "Diagnostic capillaire",
+      "Shampoing",
+      "Soin Vapeur",
+      "Séchage",
+      "Massage manuel du cuir chevelu",
+      "Coupe de transformation ou de restructuration",
+      "Mise en beauté (coiffure simple)",
+    ],
+  },
+  "rituel-soin-supreme-vapeur-coupe-des-pointes": {
+    name: `Rituel "Soin Suprême Vapeur" - Coupe des pointes`,
+    image:
+      "https://www.nefhairdydy.com/wp-content/uploads/2023/10/Modele-Coupes-NefHairDydy-25-1024x682.jpg",
+    price: 145,
+    description: `Rituel Suprême Vapeur
+
+Ce rituel vous permet de bénéficier, d'un soin complet et profond, pour vos cheveux, du soin, à la coupe des pointes jusqu'a la coiffure, vous profiterez d'un vrai moment dé détente, avec l'expérience sous casque à vapeur pour un soin plus en profondeur.`,
+    content: [
+      "Entretien client",
+      "Diagnostic capillaire",
+      "Shampoing",
+      "Soin vapeur",
+      "Séchage",
+      "Massage manuel du cuir chevelu",
+      "Coupe des pointes",
+      "Mise en beauté",
+    ],
+  },
+  "rituel-coupe-de-transformation-ou-mise-en-forme": {
+    name: `Rituel "Coupe de transformation"`,
+    image:
+      "https://www.nefhairdydy.com/wp-content/uploads/2023/10/Modele-Coupes-NefHairDydy-5-683x1024.jpg",
+    price: 150,
+    description: `Vous souhaitez restructurer la forme de vos cheveux, ou vous souhaitez passer à une nouvelle coupe, changer de style, ou tout simplement couper vos pointes, Dydy vous accompagne dans votre démarche et vous conseille sur la meilleure alternative en matière de coupe.`,
+    content: [
+      "Entretien client",
+      "Diagnostic / Choix de la coupe",
+      "Coupe de Transformation",
+      "Shampoing",
+      "Soin",
+      "Mise en beauté",
+    ],
+  },
+  "rituel-soin-exfoliant-relaxant": {
+    name: "Rituel soin exfoliant relaxant",
+    image:
+      "https://www.nefhairdydy.com/wp-content/uploads/2023/10/Modele-Coupes-NefHairDydy-24-683x1024.jpg",
+    price: 125,
+    description: `Rituel Soin Exfoliant/Relaxant (pellicules, desquamations)
+    Ce rituel est focalisé sur le soin du cuir chevelu, si vous rencontrez des problématiques avec votre cuir chevelu (pellicules, desquamation), Dydy vous propose une prestation personnalisée en fonction de votre problématique et de vos besoins. Une analyse de votre cuir chevelu et un soin naturel adapté à votre problématique un massage pour vous détendre et vous relaxer, et une mise en beauté.`,
+    content: [
+      "Entretien client",
+      "Diagnostic capillaire",
+      "Shampoing Clarifiant",
+      "Gommage cuir chevelu",
+      "Soin revitalisant",
+      "Massage du cuir chevelu",
+      "Mise en beauté",
+    ],
+  },
+  "rituel-retour-au-naturel-bigchop": {
+    name: "Rituel retour au naturel bigchop",
+    image:
+      "https://www.nefhairdydy.com/wp-content/uploads/2023/10/Modele-Coupes-NefHairDydy-1.jpg",
+    price: 110,
+    description: `Ce rituel vous permet de vous débarrasser des longueurs altérées par le défrisage, ou tout autre procédé chimique. Que vous souhaitiez revenir au naturel, faire une mise en forme, ou travailler votre transition, Dydy vous accompagne tout au long de votre expérience lors de votre rendez-vous en vous aidant à choisir une coupe selon votre besoin.`,
+    content: [
+      "Entretien client",
+      "Diagnostic capillaire",
+      "Coupe Bigchop (et/ou) de transformation",
+      "Shampoing",
+      "Soin classique hydratant ou exfoliant",
+      "Coiffure",
+    ],
+  },
+  "rituel-decouverte": {
+    name: `Rituel "Découverte"`,
+    image:
+      "https://www.nefhairdydy.com/wp-content/uploads/2023/10/Modele-Coupes-NefHairDydy-29.jpg",
+    price: 110,
+    description: `Le "Rendez-vous Découverte"est une expérience qui vous permet de réaliser un soin classique pour vos cheveux si vous venez pour la première fois. Si vous n'êtes jamais allé en salon, et que vous avez des a priori ou des frayeurs à confier vos cheveux à un professionnel Dydy vous accueille pour un rendez-vous personnalisé, une expérience de qualité, adaptée à votre texture et à vos besoins.`,
+    content: [
+      "Entretien client",
+      "Shampoing Hydratant",
+      "Diagnostic capillaire",
+      "Soin Hydratant/Revitalisant ",
+      "Mise en beauté (coiffure simple*)",
+    ],
+  },
+  "rituel-special-enfant": {
+    name: `Rituel "Spécial Enfant" (à partir de 7 ans)`,
+    image:
+      "https://www.nefhairdydy.com/wp-content/uploads/2024/11/nefhairdydy-prestation-enfant-03.jpeg",
+    price: 70,
+    description: `Dydy vous propose une expérience pour vos enfants, un soin adapté pour leurs cheveux en pleine croissance. Du shampoing à l’hydratation, jusqu’à la coiffure. Si vous ne savez pas comment prendre soin des cheveux de votre enfant, ou que vous rencontrez des difficultés à confier leurs cheveux pour les coiffer, Dydy vous accueille dans un cadre chaleureux en rendez-vous personnalisé, une expérience de qualité adaptée à la texture et au besoin de vos enfants, et cela sans douleurs, et sans cris.`,
+    content: [
+      "Diagnostic Capillaire (Analyseur de cheveux obligatoire)",
+      "Shampoing",
+      "Apres-Shampoing soin",
+      "Hydratation",
+      "Coiffure simple",
+    ],
+  },
 }
